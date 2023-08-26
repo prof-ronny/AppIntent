ab850a3d9191cd8cb96bed283f3bca4d59a81836
Laboratórios - W10 - Fatec Mauá <labs@labtec.local>
2023-08-26 11:15:44 -0300

    Initial commit Criação do Projeto Intent

---
+++ app/src/main/java/ronny/sousa/app_Ger_Int/Tela2Activity.kt
@@ -0,0 +1,11 @@
+package ronny.sousa.app_Ger_Int
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+    }
+}
